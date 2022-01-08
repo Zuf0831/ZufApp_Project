f0bd897c8fd0d62e8520bfcb496ea2b3b3991721
Zuf0831 <yusufsulle3108@gmail.com>
2022-01-08 19:17:24 +0700

    Add logo for the app

--- CovidCaseIndo.py
+++ CovidCaseIndo.py
@@ -7,6 +7,7 @@ from streamlit.script_runner import StopException, RerunException
 from plotly.offline import iplot
 import plotly.graph_objs as go
 import plotly.express as px
+from PIL import Image
 
 
 #create empty figure and we will creating data into it
@@ -14,7 +15,8 @@ fig = go.Figure()
 
 #Make some style using st framework to display it in website
 st.title('Covid 19 Tracking App ')
-
+logo = Image.open('logo.png')
+st.image(logo, width = 300)
 st.markdown("""Coronavirus is officially a pandemic. Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
                 Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment. However, some will become seriously ill and require medical attention.
                 This app created to show and check covid cases status in indonesia!
