6b5a7d2fca4510d75bdb3f4a84e84791c8c75084
Zuf0831 <yusufsulle3108@gmail.com>
2022-01-09 12:59:34 +0700

    Testing function to return data by a city or key

--- tes.py
+++ tes.py
@@ -1,11 +1,26 @@
 import pandas as pd
 import requests
-from pandas.io.json import json_normalize
+from pandas import json_normalize
 
 
 url = 'https://data.covid19.go.id/public/api/prov.json'
 x = requests.get(url)
-d = json_normalize(x.json(),['list_data'])
+d = json_normalize(x.json())
 # cd = pd.read_json(d).head()
-z = d['jumlah_kasus']
-print(z)
+# z = d['jumlah_kasus']
+# print(z)
+
+ids = []
+to_find = input('Cari kasus by kota : ')
+
+def search_case(citu):
+    for item in d['list_data'] :
+        for s in range(len(item)):
+            city = item[s]["key"]
+            kasus = item[s]["jumlah_kasus"]
+            if citu == city :
+                return kasus
+
+print(" Hasil : ", search_case(to_find))
+
+        
